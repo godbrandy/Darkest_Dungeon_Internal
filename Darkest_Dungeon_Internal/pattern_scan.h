9d78582767604ea9fe8c3e66241388f4eb40d168
godbrandy <125593220+godbrandy@users.noreply.github.com>
2023-09-22 00:28:00 +0200

    added comments and deleted commented out code

--- Darkest_Dungeon_Internal/pattern_scan.h
+++ Darkest_Dungeon_Internal/pattern_scan.h
@@ -2,8 +2,3 @@
 #include "pch.h"
 
 void* PatternScan(char* base, size_t size, const char* pattern, const char* mask);
-
-void* PatternScanEx(HANDLE hProcess, uintptr_t begin, uintptr_t end, char* pattern, char* mask);
-
-void* PatternScanExModule(HANDLE hProcess, wchar_t* exeName, wchar_t* module, char* pattern, char* mask);
-
